f17a5392e344d971ae300761d6757c168c650f95
Risha <browny1320@mail.com>
2022-08-15 14:47:51 +0300

    added new code

--- Program.cs
+++ Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿ int ReadInt(string message)
+ {
+    Console.WriteLine(message);
+    return Convert.ToInt32(Console.ReadLine());
+
+ }
+
+ int number = ReadInt("Введите трехзначное число ");
+ Console.WriteLine(DeleteSecondDigit(number));
+ int DeleteSecondDigit(int number)
+ {
+    int first = number / 10;
+    int second = first % 10;
+    return second;
+ }
